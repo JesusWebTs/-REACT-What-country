a730a2d57196f07ebea1d44767fa3b3d6286e993
xzotacx <jesus.152006@gmail.com>
2022-05-13 23:55:53 -0400

    terminando desafio kimchallenge

--- respuesta_pregunta_tecnica/img1.png/index.js
+++ respuesta_pregunta_tecnica/img1.png/index.js
@@ -1,36 +1,26 @@
-# Desafío para Software Engineers
-
-Nombre postulante: [Richard de Jesus Martinez Shildes]
-Link a la app en producción: [https://whatcountry.vercel.app/]
-
-## Respuesta A LA pregunta
-
-### Pregunta
-"La tabla que contiene la información correspondiente a la asistencia diaria de un niño en un colegio tiene 90 millones de filas. Todas las tablas del sistema existen en la misma BDD en MySQL. La lógica del backend que actualiza la información correspondiente al pasar la asistencia tiene un tiempo de servicio p95 de 10 segundos. El equipo está interesado en bajar este tiempo para mejorar la experiencia del usuario (y porque nos gusta pensar en Kimche como un Ferrari). ¿Qué propondrías para enfrentar el problema? Esta pregunta es abierta, no hay respuestas malas. Puedes proponer arquitectura, tecnologías, diseño, etc."
-
-### Respuesta
-
-#### BUSQUESA BINARIA
-
-Tiempo de ejecucion logaritmico
-Condiciones
- Tener al menos un campo con un orden secuencial establecido por ejemplo 1n+1
-Ventajas
- Velocidad al momento de conseguir informacion en objetos muy grandes.
- El tiempo de respuesta sera similar para cualquier busquedas.
-Desventajas
- Tardara aproximadamente el mismo tiempo en retornar informacion de los primeros resoltados tanto como los ultimos resultados.
-Secuencia.
- Estableciendo N = 90.000.000 siendo la secuencia de id 1n+1
- Y el target T = 4.589.635
- I el numero de iteracion
- N/2 < target
-     N = N/2 ..... N => Repite la secuencia
- N/2 > target
-     N = 0 ......N/2 => Repite la secuencia
- N=target
-     devuelve la inforamcion del target.
-
+/** BUSQUESA BINARIA
+ * Tiempo de ejecucion logaritmico
+ *
+ * Condiciones
+ *  Tener al menos un campo con un orden secuencial establecido por ejemplo 1n+1
+ * Ventajas
+ *  Velocidad al momento de conseguir informacion en objetos muy grandes.
+ *  El tiempo de respuesta sera similar para cualquier busquedas.
+ * Desventajas
+ *  Tardara aproximadamente el mismo tiempo en retornar informacion de los primeros resoltados tanto como los ultimos resultados.
+ *
+ * Secuencia.
+ *  Estableciendo N = 90.000.000 siendo la secuencia de id 1n+1
+ *  Y el target T = 4.589.635
+ *  I el numero de iteracion
+ *  N/2 < target
+ *      N = N/2 ..... N => Repite la secuencia
+ *  N/2 > target
+ *      N = 0 ......N/2 => Repite la secuencia
+ *  N=target
+ *      devuelve la inforamcion del target.
+ *
+ */
 
 let medirTiempo = (cv) => {
   let timeStart;
@@ -104,8 +94,3 @@ console.log("Busqueda Logaritmica");
 medirTiempo(() => console.log(testDeBusqueda(T, arr)));
 console.log("Busqueda Normal");
 medirTiempo(() => console.log(busquedaNormal(T, arr)));
-
-## Imagen comparativa de eficiencia.
-
-![image1](respuesta_pregunta_tecnica/img1.png)
-
